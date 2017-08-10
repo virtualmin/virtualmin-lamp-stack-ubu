a3b6ac640c3ae3b625d2fbc0580f70489e9b1631
Joe Cooper <swelljoe@gmail.com>
2017-08-10 05:50:19 -0500

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Script to add packages to aptly repos
+
+for i in *.deb; do
+  echo "Adding $i to xenial repo"
+  aptly repo add virtualmin-xenial $i
+  echo "Adding $i to trusty repo"
+  aptly repo add virtualmin-trusty $i
+done
