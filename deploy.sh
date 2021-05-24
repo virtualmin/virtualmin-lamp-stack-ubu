05b3e2b8125aa1be3643113f84855e9ab319bbc4
Joe Cooper <joe@virtualmin.com>
2021-05-24 02:31:58 +0000

    Remove EOL distros

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,6 @@
 # Script to add packages to aptly repos
 
 for i in *.deb; do
-  echo "Adding $i to xenial repo"
-  aptly repo add virtualmin-xenial $i
-  echo "Adding $i to trusty repo"
-  aptly repo add virtualmin-trusty $i
   echo "Adding $i to bionic repo"
   aptly repo add virtualmin-bionic $i
   echo "Adding $i to focal repo"
